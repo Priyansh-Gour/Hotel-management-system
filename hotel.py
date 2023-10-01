e186c09d5882d7bdf480d46cc2fdebdecd842fc2
Priyansh-Gour <priyanshgour817@gmail.com>
2023-10-01 14:52:43 +0530

    third commit

--- hotel.py
+++ hotel.py
@@ -11,7 +11,7 @@ class HotelManagementSystem:
         self.root.geometry('1550x800+0+0')
 
         # First image
-        img1=Image.open(r"C:\Users\priya\OneDrive\Desktop\My programs\py.programs\hms\hotel\hotel1.png")
+        img1=Image.open(r"C:\Users\priya\OneDrive\Desktop\My programs\py.programs\Hotel Management system\images\hotel1.webp")
         img1=img1.resize((1550,140),Image.LANCZOS)
         self.photoimg1=ImageTk.PhotoImage(img1)
 
@@ -19,11 +19,11 @@ class HotelManagementSystem:
         lable_img1.place(x=0,y=0,width=1550,height=140)
 
         # Logo
-        img2=Image.open(r"C:\Users\priya\OneDrive\Desktop\My programs\py.programs\hms\hotel\logohotel.png")
+        img2=Image.open(r"C:\Users\priya\OneDrive\Desktop\My programs\py.programs\Hotel Management system\images\Logo.webp")
         img2=img2.resize((230,140),Image.LANCZOS)
-        self.photoimg2=ImageTk.PhotoImage(img1)
+        self.photoimg2=ImageTk.PhotoImage(img2)
 
-        lable_img2=Label(self.root,image=self.photoimg1,bd=4,relief=RIDGE)
+        lable_img2=Label(self.root,image=self.photoimg2,bd=4,relief=RIDGE)
         lable_img2.place(x=0,y=0,width=230,height=140)
 
 
